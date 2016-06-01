533b67a14ec427be92c6d640a7c3c1a34dda1a6f
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2016-06-01 23:37:44 +0200

    Step 1-3 Tutorial

---
+++ app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } 		from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS }
+							from '@angular/router-deprecated';
+
+import { HeroService } 		from './hero.service';
+import { HeroesComponent } 	from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<h1>{{title}}</h1>
+		<nav>
+			<a [routerLink]="['Dashboard']">Dashboard</a>
+			<a [routerLink]="['Heroes']">Heroes</a>
+		</nav>
+		<router-outlet></router-outlet>
+	`,
+	directives: [ROUTER_DIRECTIVES],
+	providers: [
+		ROUTER_PROVIDERS,
+		HeroService
+	]
+})
+@RouteConfig([
+	{
+		path: '/heroes',
+		name: 'Heroes',
+		component: HeroesComponent
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: DashboardComponent,
+		useAsDefault: true
+	}
+])
+export class AppComponent {
+	title = 'Tour of Heroes';
+}
+
